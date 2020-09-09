fb5ab8121b689883f6795835f2385ac75f6350fb
dark <473380917@qq.com>
2020-09-09 15:02:52 +0800

    deploy

---
+++ precache-manifest.15f67e2a52aa9b84c40c27ac4230069d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7fbef1197a30259ca67",
+    "url": "/Account-book-website/css/app.eca910ab.css"
+  },
+  {
+    "revision": "1b476592404bf5076b51a571bcc62b66",
+    "url": "/Account-book-website/index.html"
+  },
+  {
+    "revision": "e7fbef1197a30259ca67",
+    "url": "/Account-book-website/js/app.c7419f54.js"
+  },
+  {
+    "revision": "be642fbdbb3ccef9137d",
+    "url": "/Account-book-website/js/chunk-vendors.e1841342.js"
+  },
+  {
+    "revision": "4b700bda4a3f05b842705dfbb2508bee",
+    "url": "/Account-book-website/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/Account-book-website/robots.txt"
+  }
+]);
